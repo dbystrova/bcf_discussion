d948dc481ce7936fceaec1e0dd590ee9d3af0f0f
bystrova <bystrova.daria@gmail.com>
2020-08-06 00:12:02 +0200

    update final plots

--- bcf_simulation.R
+++ bcf_simulation.R
@@ -5,6 +5,7 @@ library(bcf)
 library(latex2exp)
 library(viridis)
 library(bartCause)
+##change directory for functions.R file
 source('~/Documents/GitHub/bcf_discussion/functions.R')
 p=2
 n = 250
@@ -28,28 +29,23 @@ x2<-seq(0,1, by=(2/(n_s - 1)))
 mu_<- outer(x1,x2,Vectorize(mu_function))
 pi_1 <-  outer(x1,x2,Vectorize(pi_function),alpha=1)
 #(q- pnorm(-3))*(6/(pnorm(3)- pnorm(-3)) 
-
 pi_05 <-  outer(x1,x2,Vectorize(pi_function),alpha=0.5)
-
-
 pi_0 <-  outer(x1,x2,Vectorize(pi_function),alpha=0)
 
 # applying smoothing to z matrix of integers
-
 #pi_05 = lissage(pi_05) # do it about ten times :-)
 
-
-pdf(file="mu.pdf",width=5,height=3)
+pdf(file="plots/mu.pdf",width=5,height=3)
 build3ds1(x1,x2,mu_,par1= expression(paste(italic(mu))) )
 dev.off()
-pdf(file="pi_1.pdf",width=10,height=9)
+pdf(file="plots/pi_1.pdf",width=10,height=9)
 build3ds1(x1,x2,pi_1,z_lim=c(0,1),par1= expression(paste(italic(alpha)," = 1")) )
 dev.off()
-pdf(file="pi_05.pdf",width=10,height=9)
+pdf(file="plots/pi_05.pdf",width=10,height=9)
 build3ds1(x1,x2,pi_05,z_lim=c(0,1),par1= expression(paste(italic(alpha)," = 0.5")))
 dev.off()
 
-pdf(file="pi_0.pdf",width=10,height=9)
+pdf(file="plots/pi_0.pdf",width=10,height=9)
 build3ds1(x1,x2,pi_0,z_lim=c(0,1),par1= expression(paste(italic(alpha)," = 0")))
 dev.off()
 
@@ -62,7 +58,7 @@ for(i in 1:20){
 }
 print(b)
 
-pdf(file="pi_vs_mu.pdf",width=5,height=3)
+pdf(file="plots/pi_vs_mu.pdf",width=5,height=3)
 print(b+xlab(TeX(sprintf('$\\mu$')))+ylab(TeX(sprintf('$\\pi$'))) +theme_bw())
 dev.off()
 
@@ -76,7 +72,7 @@ for(i in 1:length(alpha_seq)){
                      data = tibble(mu = mu, pi = alpha_seq[i]*(0.8*pnorm(mu/ (0.1*(2-x_tilde) +0.25) ) +0.025*(x_tilde)) +0.05*(0.5*(19 - 17*alpha_seq[i]))), color=alpha_colors[i])
 }
 
-pdf(file="pi_vs_mu_alpha.pdf",width=5,height=3)
+pdf(file="plots/pi_vs_mu_alpha.pdf",width=5,height=3)
 print(b+xlab(TeX(sprintf('$\\mu$')))+ylab(TeX(sprintf('$\\pi$'))) +theme_bw())
 dev.off()
 
@@ -123,24 +119,11 @@ simulation <- function(i,alpha ){
   RMSE= sqrt(sum((1+ tauhat)^2)/n)
   ## bias
   bias= mean(tauhat + 1)
-  ## coverage
-  isCovered <- function(i){
-    ifelse(tau_ests$Low95[i] <= tau & tau <= tau_ests$Up95[i], 1, 0)
-  }
-  coverage <- lapply(1:length(tau_ests$Mean), isCovered)
-  perCoverage <- sum(unlist(coverage))/length(tau_ests)
   bcf_ate = mean(tauhat)
-  coverage_bcf =  ifelse(tau_ate$Low95<= -tau & -tau <=tau_ate$Up95, 1, 0)
   #Bart
   bartc1 <- bartc(response = y, treatment = z, confounders = x, method.rsp = "bart", method.trt = "none", p.scoreAsCovariate = FALSE,n.chains=2,n.samples = 2000)
   bart_ate = unlist(summary(bartc1)[9]$estimates[1])
-  #ites <- extract(bartc1, type = "ite")
-  #tau_x  = apply(ites, 2, mean)
-  #RMSE_bart= sqrt(sum((1 - tau_x)^2)/n)
-  #RMSE_bart
-  coverage_bart =  as.numeric(ifelse(summary(bartc1)[9]$estimates[3]<= -tau & -tau <=summary(bartc1)[9]$estimates[4], 1, 0))
-  return( tibble(i=i, bcf_ate =bcf_ate,bcf_coverage =coverage_bcf,bcf_cover =perCoverage, bcf_bias = bias, bcf_rmse= RMSE,bart_ate = bart_ate, bart_coverage = coverage_bart))
- # return(list(bcf = list(bcf_ate =bcf_ate,bcf_coverage =coverage_bcf,bcf_cover =perCoverage, bcf_bias = bias, bcf_rmse= RMSE), bart= list(bart_ate = bart_ate, bart_coverage = coverage_bart))) 
+  return( tibble(i=i, bcf_ate = bcf_ate, bart_ate = bart_ate))
 }
 
 alpha_seq <- seq(0, 1, by = 0.2)
@@ -150,7 +133,6 @@ for (j in 1:length(alpha_seq)){
   for (i in 1:50){
    dat <- simulation(j*50 + i, alpha =alpha_seq[j])
    dat$alpha = alpha_seq[j]
-   #rownames(dat) <- NULL
    datalist[[i]] <- as.data.frame(dat)
    print(c(i,j))
   }
@@ -169,7 +151,7 @@ for (j in 1:length(alpha_seq)){
 
 df_alpha_ <-  do.call(rbind, data_alpha_list)
 #save(df_alpha_, file = "alpha_100.Rdata")
-pdf(file="bias.pdf",width=5,height=3)
+pdf(file="plots/bias.pdf",width=5,height=3)
 df_alpha_[, c("alpha","bias_bart", "bias_bcf")]%>% gather(Model, bias, bias_bart:bias_bcf)%>%
   ggplot(aes(x=alpha,y=bias,col=Model))+geom_line(alpha=0.7)+ scale_color_viridis(discrete=TRUE)+
   xlab(TeX(sprintf('$\\alpha$')))+ylab("Bias") +theme_bw() +
@@ -179,7 +161,7 @@ df_alpha_[, c("alpha","bias_bart", "bias_bcf")]%>% gather(Model, bias, bias_bart
 
 dev.off()
 
-pdf(file="rmse.pdf",width=5,height=3)
+pdf(file="plots/rmse.pdf",width=5,height=3)
 df_alpha_[, c("alpha","fin_rmse_bart", "fin_rmse_bcf")]%>% gather(Model, Rmse, fin_rmse_bart:fin_rmse_bcf)%>%
   ggplot(aes(x=alpha,y=Rmse,col=Model))+geom_line(alpha=0.7)+ scale_color_viridis(discrete=TRUE)+
   #labs(title="Bias")+
@@ -201,10 +183,9 @@ for (i in 1:(12)){
   if (grepl("bcf",dq10$Model[i], fixed = TRUE)){dq10$Model[i] = "bcf"}
   if (grepl("bart",df_bias$Model[i], fixed = TRUE)){df_bias$Model[i] = "bart"}
   if (grepl("bcf",df_bias$Model[i], fixed = TRUE)){df_bias$Model[i] = "bcf"}
-  
 }
 # 
-pdf(file="bias_ribbon.pdf",width=5,height=3)
+pdf(file="plots/bias_ribbon.pdf",width=5,height=3)
 bias_all <- cbind(df_bias, dq10[, "q10"], dq90[, "q90"])
 bias_all%>% ggplot(aes(x=alpha,y=bias,col=Model))+geom_line(alpha=0.9, size=1)+ geom_ribbon(aes(ymin = q10 + 1, ymax = q90 + 1),linetype = 2,alpha=0.0) +
   xlab(TeX(sprintf('$\\alpha$')))+ylab("bias") +theme_bw() +theme(legend.position="none")
