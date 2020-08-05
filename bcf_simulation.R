add3d2c5ecd2a8ec99649619cf6ff3d224f772c7
bystrova <bystrova.daria@gmail.com>
2020-08-05 17:22:13 +0200

    update

--- bcf_simulation.R
+++ bcf_simulation.R
@@ -83,7 +83,7 @@ dev.off()
 
 
 simulation <- function(i,alpha ){
-  set.seed(i +1)
+  set.seed(i+1)
   #### control variables matrix x_i ~ Unif(0,1)
   x = matrix(runif(n*p, min = 0, max = 1), nrow=n)
   # create targeted selection
@@ -91,7 +91,6 @@ simulation <- function(i,alpha ){
   # generate treatment variable
   # probability of recievng the treatment
   pi = alpha*(0.8* pnorm(q/ (0.1*(2-x[,1]- x[,2]) +0.25 )) + 0.025*(x[,1] +x[,2])) +0.05*(0.5*(19 - 17*alpha))
-  #pi = alpha*(0.8* pnorm(q/ (0.05*(4-x[,1]- x[,2]) +0.25 )) + 0.0125*(x[,1]+ x[,2]) ) +0.05*(0.5*(19 - 17*alpha))
   #treatment
   z = rbinom(n,1,pi)
   # tau is the true (homogeneous) treatment effect
@@ -105,8 +104,9 @@ simulation <- function(i,alpha ){
   y = mu + sigma*rnorm(n)
   
   # If you didn't know pi, you would estimate it here
-  pihat = pnorm(q)
-  
+  #pihat = pnorm(q)
+  pihat = pi
+
   bcf_fit = bcf(y, z, x,x, pihat, nburn=n_burn, nsim=n_sim,include_pi= "control")
   ## convergence assessment for BCF
   #summary(bcf_fit)
@@ -131,15 +131,15 @@ simulation <- function(i,alpha ){
   coverage <- lapply(1:length(tau_ests$Mean), isCovered)
   perCoverage <- sum(unlist(coverage))/length(tau_ests)
   bcf_ate = mean(tauhat)
-  coverage_bcf =  ifelse(tau_ate$Low95<= tau & tau <=tau_ate$Up95, 1, 0)
+  coverage_bcf =  ifelse(tau_ate$Low95<= -tau & -tau <=tau_ate$Up95, 1, 0)
   #Bart
-  bartc1 <- bartc(response = y, treatment = z, confounders = x, method.rsp = "bart", method.trt = "none", n.chains=4, n.samples = 2000)
+  bartc1 <- bartc(response = y, treatment = z, confounders = x, method.rsp = "bart", method.trt = "none", p.scoreAsCovariate = FALSE,n.chains=2,n.samples = 2000)
   bart_ate = unlist(summary(bartc1)[9]$estimates[1])
   #ites <- extract(bartc1, type = "ite")
   #tau_x  = apply(ites, 2, mean)
   #RMSE_bart= sqrt(sum((1 - tau_x)^2)/n)
   #RMSE_bart
-  coverage_bart =  as.numeric(ifelse(summary(bartc1)[9]$estimates[3]<= tau & tau <=summary(bartc1)[9]$estimates[4], 1, 0))
+  coverage_bart =  as.numeric(ifelse(summary(bartc1)[9]$estimates[3]<= -tau & -tau <=summary(bartc1)[9]$estimates[4], 1, 0))
   return( tibble(i=i, bcf_ate =bcf_ate,bcf_coverage =coverage_bcf,bcf_cover =perCoverage, bcf_bias = bias, bcf_rmse= RMSE,bart_ate = bart_ate, bart_coverage = coverage_bart))
  # return(list(bcf = list(bcf_ate =bcf_ate,bcf_coverage =coverage_bcf,bcf_cover =perCoverage, bcf_bias = bias, bcf_rmse= RMSE), bart= list(bart_ate = bart_ate, bart_coverage = coverage_bart))) 
 }
@@ -154,50 +154,77 @@ for (j in 1:length(alpha_seq)){
    #dat =  column_to_rownames(dat, var = "i")
    rownames(dat) <- NULL
    datalist[[i]] <- as.data.frame(dat)
+   print(c(i,j))
   }
   df = do.call(rbind, datalist)
   dat_alpha = tibble(alpha =alpha_seq[j],
-                    fin_rmse_bart = sqrt(mean((unlist(df$bart_ate ) + 1)^2)),
-                    fin_rmse_bcf = sqrt(mean((df$bcf_ate  + 1)^2)),
-                    bias_bart= mean(unlist(df$bart_ate ) + 1),
-                    bias_bcf =  mean(df$bcf_ate  + 1),
-                    bart_coverage = sum(df$bart_coverage)/length(df$bart_coverage),
-                    bcf_coverage = sum(df$bcf_coverage)/length(df$bcf_coverage))
+                     fin_rmse_bart = sqrt(mean((unlist(df$bart_ate ) + 1)^2)),
+                     fin_rmse_bcf = sqrt(mean((df$bcf_ate  + 1)^2)),
+                     bias_bart= mean(unlist(df$bart_ate ) + 1),
+                     bias_bcf =  mean(df$bcf_ate  + 1),
+                     ate_bart_q10 = quantile(df$bart_ate, 0.1),
+                     ate_bart_q90 = quantile(df$bart_ate, 0.9),
+                     ate_bcf_q10 = quantile(df$bcf_ate, 0.1),
+                     ate_bcf_q90 = quantile(df$bcf_ate, 0.9))
   data_alpha_list[[j]]<- dat_alpha
 }
 
 df_alpha_ <-  do.call(rbind, data_alpha_list)
 
-#pdf(file="bias.pdf",width=5,height=3)
+pdf(file="bias.pdf",width=5,height=3)
 df_alpha_[, c("alpha","bias_bart", "bias_bcf")]%>% gather(Model, bias, bias_bart:bias_bcf)%>%
   ggplot(aes(x=alpha,y=bias,col=Model))+geom_line(alpha=0.7)+ scale_color_viridis(discrete=TRUE)+
   #labs(title="Rmse")+
-  xlab(TeX(sprintf('$\\alpha$')))+ylab("bias") +theme_bw()
-#dev.off()
+  xlab(TeX(sprintf('$\\alpha$')))+ylab("bias") +theme_bw() +theme(legend.position="none")
+dev.off()
 
-#pdf(file="rmse.pdf",width=5,height=3)
+pdf(file="rmse.pdf",width=5,height=3)
 df_alpha_[, c("alpha","fin_rmse_bart", "fin_rmse_bcf")]%>% gather(Model, Rmse, fin_rmse_bart:fin_rmse_bcf)%>%
   ggplot(aes(x=alpha,y=Rmse,col=Model))+geom_line(alpha=0.7)+ scale_color_viridis(discrete=TRUE)+
   #labs(title="Bias")+
-  xlab(TeX(sprintf('$\\alpha$')))+ylab("RMSE") +theme_bw()
-#dev.off()
+  xlab(TeX(sprintf('$\\alpha$')))+ylab("RMSE") +theme_bw()+ theme(legend.position="none")
+dev.off()
+
+
+
+df_bias <- df_alpha_[, c("alpha","bias_bart", "bias_bcf")]%>% gather(Model, bias, bias_bart:bias_bcf)
+dq10 <- df_alpha_[, c("alpha","ate_bart_q10", "ate_bcf_q10")]%>% gather(Model, q10, ate_bart_q10:ate_bcf_q10)
+dq90 <- df_alpha_[, c("alpha","ate_bart_q90", "ate_bcf_q90")]%>% gather(Model, q90, ate_bart_q90:ate_bcf_q90)
+for (i in 1:(12)){
+  if (grepl("bart",dq90$Model[i], fixed = TRUE)) {dq90$Model[i] = "bart"}
+  if (grepl("bcf",dq90$Model[i], fixed = TRUE)){dq90$Model[i] = "bcf"}
+  if (grepl("bart",dq10$Model[i], fixed = TRUE)){dq10$Model[i] = "bart"}
+  if (grepl("bcf",dq10$Model[i], fixed = TRUE)){dq10$Model[i] = "bcf"}
+  if (grepl("bart",df_bias$Model[i], fixed = TRUE)){df_bias$Model[i] = "bart"}
+  if (grepl("bcf",df_bias$Model[i], fixed = TRUE)){df_bias$Model[i] = "bcf"}
+  
+}
+
+pdf(file="bias_ribbon.pdf",width=5,height=3)
+bias_all <- cbind(df_bias, dq10[, "q10"], dq90[, "q90"])
+bias_all%>% ggplot(aes(x=alpha,y=bias,col=Model))+geom_line(alpha=0.9, size=1)+ geom_ribbon(aes(ymin = q10 + 1, ymax = q90 + 1),linetype = 2,alpha=0.0) +
+  xlab(TeX(sprintf('$\\alpha$')))+ylab("bias") +theme_bw() +theme(legend.position="none")
+dev.off()
 
 
 
 
 
-library(dbarts)
-bartFit<- bart(x, y, nskip = 350, ntree = 1000)
-plot(bartFit)
+# 
+# library(dbarts)
+# bartFit<- bart(x, y, nskip = 350, ntree = 1000)
+# plot(bartFit)
+# 
+# df2 <- data.frame(df, 
+#                   ql = apply(bartFit$yhat.train, length(dim(bartFit$yhat.train)), quantile,probs=0.05),
+#                   qm = apply(bartFit$yhat.train, length(dim(bartFit$yhat.train)), quantile,probs=.5),
+#                   qu <- apply(bartFit$yhat.train, length(dim(bartFit$yhat.train)), quantile,probs=0.95)
+# )
+# 
+# bartp <- ggplot(df2, aes(x= y, y = qm)) + geom_linerange(aes(ymin = ql, ymax = qu), col = "grey") +
+#   geom_point() + geom_smooth() +
+#   geom_abline(intercept = 0, slope = 1, col = "red", size = 1)
+# 
+# bartp
+# 
 
-df2 <- data.frame(df, 
-                  ql = apply(bartFit$yhat.train, length(dim(bartFit$yhat.train)), quantile,probs=0.05),
-                  qm = apply(bartFit$yhat.train, length(dim(bartFit$yhat.train)), quantile,probs=.5),
-                  qu <- apply(bartFit$yhat.train, length(dim(bartFit$yhat.train)), quantile,probs=0.95)
-)
-
-bartp <- ggplot(df2, aes(x= y, y = qm)) + geom_linerange(aes(ymin = ql, ymax = qu), col = "grey") +
-  geom_point() + geom_smooth() +
-  geom_abline(intercept = 0, slope = 1, col = "red", size = 1)
-
-bartp
